5041c576876a75a60cc745b6cff4cdc438a99a73
Kishoripathrabe <kishoripathrabe123@gmail.com>
2022-12-17 19:10:02 +0530

    component template and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [`
+  h1 {
+    color: blue;
+  }`
+] // now it will take array of strings.
 })
 export class AppComponent {
   title = 'angular-demo';
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `<app-server></app-server>
+      Hey !
+    <app-server></app-server>` //for multiline expression use backtick (` `).
+    // '<app-server></app-server><app-server></app-server>' if you dont want to write the content in it.
+    ,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
